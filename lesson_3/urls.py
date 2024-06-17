6ca55b62c6526f48f106de13bd797942870f94a5
SShGitHubUser <shypulin@ukr.net>
2024-06-18 00:00:03 +0300

    Lesson 3 tasks updated

--- lesson_3/urls.py
+++ lesson_3/urls.py
@@ -4,9 +4,26 @@ from . import views
 urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
     path('download/', views.download, name='download'),
-    # path('weather/', views.openweathermap, name='owm'),
+
+    # Taqsk 3. Function Based Views
+    path('func_file/', views.func_file, name='func_file'),
+    path('func_json/', views.func_json, name='func_json'),
+    path('func_html/', views.func_html, name='func_html'),
+    path('func_text/', views.func_text, name='func_text'),
+    # Taqsk 3. Class Based Views
+    path('class_file/', views.ClassFile.as_view(), name='class_file'),
+    path('class_json/', views.ClassJSON.as_view(), name='class_json'),
+    path('class_html/', views.ClassHTML.as_view(), name='class_html'),
+    path('class_text/', views.ClassText.as_view(), name='class_text'),
+
     path('sort_tasks/', views.sort_task_list, name='sort_task_list'),
-    # path('weather/', views.openweathermap, name='owm'),
+
+    # Taqsk 5. Star Wars
+    path('star_wars/', views.star_wars, name='index'),
+    path('star_wars/luke/', views.luke, name='luke'),
+    path('star_wars/leia/', views.leia, name='leia'),
+    path('star_wars/han/', views.han, name='han'),
+
     path('persons/', views.person_list, name='person_list'),
     path('polls/', views.polls, name='polls'),
 ]
--- lesson_3/views.py
+++ lesson_3/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+import os
+
+from django.http import HttpResponse, FileResponse, JsonResponse
 from django.shortcuts import render
+from django.views import View
+
+from Django import settings
 
 lets_do_it = [{'priority': 100, 'task': 'Скласти список справ'},
               {'priority': 150, 'task': 'Вчити Django'},
@@ -20,9 +25,65 @@ def download(request):
     return response
 
 
+def func_file(request):
+    file_path = os.path.join(settings.BASE_DIR, 'manage.py')
+    return FileResponse(open(file_path, 'rb'), content_type='application/py')
+
+
+def func_json(request):
+    data = {'message': 'This is a func JSON response'}
+    return JsonResponse(data)
+
+
+def func_html(request):
+    return HttpResponse('<h3>This is a func HTML response</h3>')
+
+
+def func_text(request):
+    return HttpResponse("This is a func text response", content_type="text/plain")
+
+
+class ClassFile(View):
+    def get(self, request, *args, **kwargs):
+        file_path = os.path.join(settings.BASE_DIR, 'manage.py')
+        return FileResponse(open(file_path, 'rb'), content_type='application/py')
+
+
+class ClassJSON(View):
+    def get(self, request, *args, **kwargs):
+        data = {'message': 'This is a class JSON response'}
+        return JsonResponse(data)
+
+
+class ClassHTML(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('<h3>This is a func HTML response</h3>')
+
+
+class ClassText(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a class text response", content_type="text/plain")
+
+
 def sort_task_list(request):
     context = {'tasks': lets_do_it}
     return render(request, 'lesson_3/task_4_sort_task_list.html', context)
+
+
+def star_wars(request):
+    return render(request, 'lesson_3/task_5_index.html')
+
+
+def luke(request):
+    return render(request, 'lesson_3/task_5_luke.html')
+
+
+def leia(request):
+    return render(request, 'lesson_3/task_5_leia.html')
+
+
+def han(request):
+    return render(request, 'lesson_3/task_5_han.html')
 
 
 def person_list(request):
