61b724523a336cfb07d744f6d225594470d5b326
SShGitHubUser <shypulin@ukr.net>
2024-07-24 23:24:30 +0300

    Some bugs fixed

--- lesson_5/urls.py
+++ lesson_5/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'lesson_6'
+app_name = 'lesson_5'
 
 urlpatterns = [
     path('', views.index, name='index'),
--- lesson_5/views.py
+++ lesson_5/views.py
@@ -3,18 +3,18 @@ from .forms import *
 
 
 def index(request):
-    with open('lesson_6/lesson_tasks_description.txt', 'rt', encoding='utf-8') as file:
+    with open('lesson_5/lesson_tasks_description.txt', 'rt', encoding='utf-8') as file:
         task_descriptions = file.read()
     context = {
         'title': "Завдання уроку 5",
         'head': "Завдання уроку 5. Форми",
         'tasks': [{'links': [],
                    'comments': ['Виконано']},
-                  {'links': [{'URL': "lesson_6:auth_forms", 'text': "http://localhost/lesson_5/auth_forms/"}],
+                  {'links': [{'URL': "lesson_5:auth_forms", 'text': "http://localhost/lesson_5/auth_forms/"}],
                    'comments': []},
-                  {'links': [{'URL': "lesson_6:model_forms", 'text': "http://localhost/lesson_5/model_forms/"}],
+                  {'links': [{'URL': "lesson_5:model_forms", 'text': "http://localhost/lesson_5/model_forms/"}],
                    'comments': []},
-                  {'links': [{'URL': "lesson_6:review_form", 'text': "http://localhost/lesson_5/review_form/"}],
+                  {'links': [{'URL': "lesson_5:review_form", 'text': "http://localhost/lesson_5/review_form/"}],
                    'comments': []},
                   ],
         'task_descriptions': task_descriptions}
